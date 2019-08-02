181fbe315f286b8cc8311920642b7c8c129fa769
informatica <informatica@BGAI202-11.bga.upb>
2019-08-02 15:06:14 -0500

    Se creó el generador de número al azar

---
+++ Desktop/testRepo/src/main/java/edu/upb/testrepo/Juego.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.upb.testrepo;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author informatica
+ */
+public class Juego {
+    public static void main(String[] args){
+        System.out.println("Dame un número entre 1 y 20");
+        Scanner teclado = new Scanner(System.in);
+        int numero = Integer.parseInt(teclado.nextLine());
+        Random r = new Random();
+        int numeroAleatorio = r.nextInt(20)+1;
+        if(numero == numeroAleatorio){
+            System.out.println("Ganaste!");
+        }else{
+            System.out.println("El número correcto era "+ numeroAleatorio + " y no "+ numero);
+        }
+    }
+}
